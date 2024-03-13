7b5bba030468a037c72e140effbc3cd4f4f70621
daniel <lesamessipa@gmail.com>
2024-03-14 00:36:40 +0100

    fix: scribe  details

--- src/app/Http/Controllers/Api/V1/Quiz/QuizController.php
+++ src/app/Http/Controllers/Api/V1/Quiz/QuizController.php
@@ -4,14 +4,12 @@ namespace App\Http\Controllers\Api\V1\Quiz;
 
 use App\Http\Controllers\Controller;
 use App\Http\Repositories\Questions\QuestionRepository;
-use App\Http\Requests\QuestionStoreRequest;
 use App\Http\Requests\QuizStoreRequest;
 use App\Http\Resources\QuizResource;
 use App\Http\Services\Questions\QuestionService;
 use App\Http\Services\Quiz\QuizService;
 use App\Models\Question;
-use App\Models\Quiz;
-use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Cache;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -25,9 +23,10 @@ class QuizController extends Controller
     /**
      * GET QUIZ
      * récupérations de tous les quiz
-     * @return \Illuminate\Http\JsonResponse
+     * @return JsonResponse
      */
-    public function index() {
+    public function index(): JsonResponse
+    {
 
         return response()->json(['data' => Cache::remember('quizzes', 60*60*24, function() {
            return  QuizResource::collection($this->quizService->getQuizzes());
@@ -35,8 +34,24 @@ class QuizController extends Controller
 
     }
 
-    /* create  a quiz */
-    public function store(QuizStoreRequest $request) {
+    /**
+     * Pour créer un quiz
+     *
+     * Utiliser les paramètres ci-dessous.
+     * Vous pouvez ajouter à la création d'un quiz l'ajout ou des questions à ce quiz ou
+     * la création des questions
+     *
+     * le paramètre questions attend une liste (tableau / objet) de question comme défini dans l'onglet de création d'une
+     * question
+     *
+     * Le paramètre questions_ids attend un tableau d'ids de question existante.
+     * @param QuizStoreRequest $request
+     * @return JsonResponse
+     *
+     *
+     */
+    public function store(QuizStoreRequest $request): JsonResponse
+    {
         if ($request->user()->cannot('create-question', Question::class)) {
             return response()->json(['message' => 'vous n\'avez pas les droits requis pour effectuer cette action.'], Response::HTTP_FORBIDDEN);
         }
