4691183ca5982ea18ad3050483dfcae641ede968
dani03 <lesamessipa@gmail.com>
2024-07-07 15:47:28 +0200

    Feature/swagger (#48)
    * add: add a new container and dockerfile for the nextJs app
    
    * fix: fix the readme details
    
    * fix: the good documentation
    
    * add: fix doc
    
    * fix: delete mysql folder
    
    * add: updated level
    
    * add: respon to a qquiz
    
    * fix: delete a quiz
    
    * add: creation link
    
    * add link
    
    * add answer crud
    
    * added route get quiz
    
    * fixed image profil picture
    
    * fix error on route get one quiz
    
    * add just route for open ai
    
    * fix issue créer question with open ai
    
    * fix description of documentation
    
    * fix connexion with google
    
    * fix length of photo
    
    * add seeders fake data
    
    * update ticket
    
    * add points
    
    * update documentation
    
    * update documentation 2
    
    * fix bug on creation role
    
    * fix show details on user answer
    
    * fix bug on resultats user on a quiz
    
    * fix bug

--- src/app/Http/Resources/QuizResource.php
+++ src/app/Http/Resources/QuizResource.php
@@ -18,24 +18,18 @@ class QuizResource extends JsonResource
     public function toArray(Request $request): array
     {
         $uniqueParticipants = $this->participants->unique('id');
-        $quiz = $this->resource['quiz'];
-
-        $question = $this->resource['question'];
-        $userAnswer = $question->answers->firstWhere('id', $this->resource['user_answer_id']);
 
         return [
-            'id' => $quiz->id,
-            'title' => $quiz->title,
-            'slug' => $quiz->slug,
-            'level_id' => $quiz->level_id,
-            'level_name' => (new LevelService(new LevelRepository()))->getLevelName($quiz->level_id),
-            'domaine_name' => (new LevelService(new LevelRepository()))->getLevelName($quiz->level_id),
-            'created_at' => Carbon::make($quiz->created_at)->diffForHumans(),
-            'updated_at' => Carbon::make($quiz->updated_at)->diffForHumans(),
-            'question' => $question->title,
-            'answers' => AnswerResource::collection($question->answers),
-            'correct_answer' => $question->answers->firstWhere('correct_answer', true)->answer,
-            'user_answer' => $userAnswer ? $userAnswer->answer : null,
+            'id' => $this->id,
+            'title' => $this->title,
+            'slug' => $this->slug,
+            'level_id' => $this->level_id,
+            'level_name' => (new LevelService(new LevelRepository()))->getLevelName($this->level_id),
+            'domaine_name' => (new LevelService(new LevelRepository()))->getLevelName($this->level_id),
+            'created_at' =>  Carbon::make($this->created_at)->diffForHumans(),
+            'updated_at' => Carbon::make($this->updated_at)->diffForHumans(),
+            'questions' => QuestionResource::collection($this->questions),
+            'candidats_list' => ProfilRessource::collection($uniqueParticipants),
         ];
     }
 }
