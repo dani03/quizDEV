346d7171a19e64e1569ddd258b9b7559e9cf65e0
dani03 <lesamessipa@gmail.com>
2024-07-15 21:55:19 +0200

    Feature/swagger (#57)
    * add: add a new container and dockerfile for the nextJs app
    
    * fix: fix the readme details
    
    * fix: the good documentation
    
    * add: fix doc
    
    * fix: delete mysql folder
    
    * add: updated level
    
    * add: respon to a qquiz
    
    * fix: delete a quiz
    
    * add: creation link
    
    * add link
    
    * add answer crud
    
    * added route get quiz
    
    * fixed image profil picture
    
    * fix error on route get one quiz
    
    * add just route for open ai
    
    * fix issue créer question with open ai
    
    * fix description of documentation
    
    * fix connexion with google
    
    * fix length of photo
    
    * add seeders fake data
    
    * update ticket
    
    * add points
    
    * update documentation
    
    * update documentation 2
    
    * fix bug on creation role
    
    * fix show details on user answer
    
    * fix bug on resultats user on a quiz
    
    * fix bug
    
    * update length
    
    * fix details error
    
    * fix updated url and link
    
    * updated some bug fixed
    
    * fix some details
    
    * fix bug
    
    * fix bug
    
    * add some tests

--- src/tests/Feature/EntrepriseTest.php
+++ src/tests/Feature/EntrepriseTest.php
@@ -2,38 +2,76 @@
 
 namespace Tests\Feature;
 
+
+use App\Models\Quiz;
 use App\Models\Role;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+
 use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
 use Tests\TestCase;
 use Faker\Factory as Faker;
 
 class EntrepriseTest extends TestCase
 {
     use RefreshDatabase;
+    private User $user;
+    private User $entrepriseUser;
     public function setUp(): void
     {
         parent::setUp();
 
-
+        $this->artisan('passport:install', ['--no-interaction' => true]);
+        $this->user = $this->createSimpleUser();
+        $this->entrepriseUser = $this->createEntrepriseUser();
     }
 
-    public function test_entreprise_has_access_to_his_fonctionnalities()
+    public function test_entreprise_index_returns_quizzes_and_entreprise_profile()
     {
+        // Create an entreprise user
 
-        $entreprise = User::factory()->create([
-            'role_id' => Role::ROLE_ENTREPRISE,
-            'lastname' => "fakerfirstName",
-            'name' => "fakerfirstName",
-            'email' => "fakerfirstName@gmail.com",
-            'password' => Hash::make('password') ,
-
-
+        // Create some quizzes associated with the entreprise user
+        $quizzes = Quiz::factory()->count(3)->create([
+            'user_id' => $this->entrepriseUser->id,
         ]);
-        $response = $this->actingAs($entreprise)->getJson('/api/v1/entreprise/quiz');
 
-        $response->assertStatus(200);
+        // Act as the entreprise user
+        $response = $this->actingAs($this->entrepriseUser, 'api')->getJson('/api/v1/entreprise/quiz');
+
+        // Assert the response status is OK
+        $response->assertStatus(Response::HTTP_OK);
+    }
+
+    public function test_classic_user_cannot_reach_entreprise_quiz()
+    {
+        // Act as the non-entreprise user
+        $response = $this->actingAs($this->user, 'api')->getJson('/api/v1/entreprise/quiz');
+
+        // l'utilisateur est un user classique donc n'a pas accès et on lui renvoie un forbidden
+        $response->assertStatus(Response::HTTP_FORBIDDEN);
+    }
+
+    private function createSimpleUser(): User
+    {
+        $faker = Faker::create();
+        return User::factory()->create([
+            'role_id' => Role::ROLE_USER,
+            'lastname' => $faker->firstName,
+            'name' => $faker->lastName,
+            'email' => $faker->email,
+            'password' => Hash::make('password'),
+        ]);
+    }
+    private function createEntrepriseUser(): User
+    {
+        $faker = Faker::create();
+        return User::factory()->create([
+            'role_id' => Role::ROLE_ENTREPRISE,
+            'lastname' => $faker->firstName,
+            'name' => $faker->lastName,
+            'email' => $faker->email,
+            'password' => Hash::make('password'),
+        ]);
     }
 }
