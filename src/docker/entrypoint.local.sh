d73442908a0a8e7abf80be49b65201ffb375c165
daniel <lesamessipa@gmail.com>
2024-07-18 19:56:50 +0200

    updated to prod

--- src/docker/entrypoint.local.sh
+++ src/docker/entrypoint.local.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-if [ ! -f "src/vendor/autoload.php" ]; then
+if [ ! -f "vendor/autoload.php" ]; then
     composer install --no-progress --no-interaction
 fi
 
-if [ ! -f "src/.env" ]; then
+if [ ! -f ".env" ]; then
     echo "Creating env file for env $APP_ENV"
-    cp src/.env.example src/.env
+    cp .env.example .env
 else
     echo "env file exists."
 fi
 
 php artisan migrate
+php artisan config:clear
+php artisan passport:install
+# php artisan db:seed
 php artisan optimize clear
 php artisan view:clear
 php artisan route:clear
